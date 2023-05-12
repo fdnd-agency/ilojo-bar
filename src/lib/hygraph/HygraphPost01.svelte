c07e24a8393588e2d4b4440efe7f614168bbcd39
M4TThts123 <70703948+M4TThys123@users.noreply.github.com>
2023-05-12 13:17:03 +0200

    Begin aan de hygraph post form

---
+++ src/lib/hygraph/HygraphPost01.svelte
@@ -0,0 +1,31 @@
+<script>
+    let title = '';
+    let description = '';
+
+    async function handleSubmit() {
+        const response = await fetch('/api/hygraph', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title, description })
+        });
+        // Handle the response from the Hygraph API
+    }
+
+</script>
+<section class="form_01">
+    <h4>Form 1</h4>
+
+    <form class="p-4 bg-light" on:submit|preventDefault={handleSubmit}>
+        <div class="form-group">
+            <label for="title">Title:</label>
+            <input type="text" id="title" class="form-control" bind:value={title}>
+        </div>
+        <div class="form-group">
+            <label for="description">Description:</label>
+            <textarea id="description" class="form-control" bind:value={description}></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</section>
---
+++ src/routes/hygraph/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+import HygraphPost01 from "$lib/hygraph/HygraphPost01.svelte";
+
+</script>
+
+<section class="container hygraph__container">
+    <h3 class="text-center">dit is de higraph container</h3>
+
+    <HygraphPost01/>
+
+</section>
+
+
+
+<style>
+    .hygraph__container{
+        margin-top: 5em;
+    }
+</style>
