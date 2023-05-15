75badc5a211a74ccd7ede24d694cce3f46686c5f
Yasser-Slaymen <yaserslyman0@gmail.com>
2023-05-16 00:35:24 +0200

    editing slide in story

--- src/svelte-carousel.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'svelte-carousel' {
-    export { default as Carousel } from 'svelte-carousel/src/Carousel.svelte';
-    export { default as Slide } from 'svelte-carousel/src/Slide.svelte';
-  }
-  
