1d87dc7bdb88c1add7c6ed34c7983fb34852dc69
Yasser-Slaymen <yaserslyman0@gmail.com>
2023-06-07 13:11:34 +0200

    editing homepage branch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
-
+<script lang="ts">
+	import './styles.css';
+	import Footer from '$lib/components/Footer.svelte';
+	import Header2 from '$lib/components/Header2.svelte';
+	import type { PageData } from './$types';
+    export let data: PageData;
+</script>
 
 <div class="app">
-
+	<Header2 stories={data.stories} />	
 	<main>
 		<slot />
 	</main>
-	
+	<Footer />
 </div>
 
 <style>
