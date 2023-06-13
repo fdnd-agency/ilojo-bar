d1fcbafa61d72ee7f56f240d4945da4589905892
M4TThts123 <70703948+M4TThys123@users.noreply.github.com>
2023-06-13 21:58:38 +0200

    Changes

--- src/lib/components/Header03.svelte
+++ src/lib/components/Header03.svelte
@@ -42,10 +42,6 @@
               <a class="nav-link" href="about">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="storypage">Help</a>
-            </li>
-           
-            <li class="nav-item">
               <a class="nav-link" href="/">Contact</a>
             </li>
           </ul>
